1aa14aa68e67b617e9006d10acfff7d601364acc
Pedro1Barbosa2 <1018068@alunos.ruizcosta.edu.pt>
2021-04-11 07:33:52 +0100

    :)

--- Login_v3/valida.php
+++ Login_v3/valida.php
@@ -4,14 +4,26 @@ session_start();
 
 $nick = $_POST['username'];
 $senha = $_POST['pass'];
-#BASE DE DADOS 
-$db = "smartmeal";
-$host = "localhost:3306";
-$user = "root";
-$pass = "";
-$con = mysqli_connect($host,$user,$pass,$db) or die ("Sem conexão ao servidor");
+#BASE DE DADOS
+#$db = "smartmeal";
+#$host = "localhost:3306";
+#$user = "root";
+#$pass = "";
+#con = mysqli_connect($host,$user,$pass,$db) or die ("Sem conexão ao servidor");
 #BASE DE DADOS
 
+#$servername = "ERCERC-0E66ECC8";
+$servername = "DESKTOP-1DOM035\SQLEXPRESS";
+
+$connectinfo = array( "Database"=>"smartmeal");
+$conn = sqlsrv_connect($servername,$connectinfo);
+
+if($conn){
+    #echo "coneection bom" . "<br>";
+}else{
+    die( print_r( sqlsrv_errors(), true));
+}
+/*
 $result = mysqli_query($con, "SELECT * FROM `login`
 WHERE `nick` = '$nick' AND `pass` = '$senha'");
 
@@ -23,5 +35,20 @@ if(mysqli_num_rows($result) > 0){
     unset ($_SESSION['email_login']);
     unset ($_SESSION['senha_login']);
     header('location:login.php?erro=1');
+}*/
+
+$tsql = "SELECT * FROM `user_login`";
+$stmt = sqlsrv_query( $conn, $tsql, array(), array( "Scrollable" => SQLSRV_CURSOR_KEYSET ));
+
+if($stmt == true){
+    $_SESSION['nick'] = $nick;
+    $_SESSION['senha_login'] = $senha;
+    header('location:homeadmin.php');
+}else{
+    unset ($_SESSION['email_login']);
+    unset ($_SESSION['senha_login']);
+    header('location:login.php?erro=1');
+    die();
 }
+
 ?>
