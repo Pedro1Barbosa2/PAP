286006ccdd84429e46d1293e235c96f97840bc40
Pedro1Barbosa2 <71782672+Pedro1Barbosa2@users.noreply.github.com>
2021-09-17 19:04:04 +0100

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -18,8 +18,8 @@ if(isset($_POST['name'])&&isset($_POST['email'])){
     $mail->isSMTP();
     $mail->Host = "smtp.gmail.com";
     $mail->SMTPAuth = true;
-    $mail->Username = "smartmealinfo4@gmail.com";
-    $mail->Password = 'Pb123465!';
+    $mail->Username = YourEmailHere;
+    $mail->Password = YourPasswordHere;
     $mail->Port = 465;
     $mail->SMTPSecure = "ssl";
 
